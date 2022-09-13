af0443c4a1049ce811bb61899ea2ad68acc0cca3
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-12 19:37:17 -0500

    pseudo-code

---
+++ game.js
@@ -0,0 +1,26 @@
+//define Rock / Paper / Scissors
+var choice = ['r', 'R' ,'p', 'P', 's', 'S']
+
+//define variable for computerChoice
+var robot = 
+//define variable for humanChoice
+
+//define variables for humans (wins, losses, ties)
+
+//prompt playerChoice
+
+//prompt randomly computerChoice
+
+//compare choices
+
+
+//display results (won , tied or lost the round)
+
+//display total stats (wins, Losses, Ties)
+
+//ask to play again
+    //if play agin (restart)
+    //else stop game
+
+
+
