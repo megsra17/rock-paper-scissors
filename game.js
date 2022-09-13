62b4e9561ce4ebbc162f292651a9b79b0263a91c
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-12 20:08:41 -0500

    code

--- game.js
+++ game.js
@@ -1,18 +1,33 @@
 //define Rock / Paper / Scissors
-var choice = ['r', 'R' ,'p', 'P', 's', 'S']
-
+var choice = ['R' , 'P', 'S']
 //define variable for computerChoice
-var robot = 
+var robot = Math.floor(Math.random() * choice.length);
 //define variable for humanChoice
-
+//var humanChoice = 
 //define variables for humans (wins, losses, ties)
-
+var w = 0;
+var l = 0; 
+var t = 0;
 //prompt playerChoice
-
+var person = prompt('Enter R, P, or S:');
+person = person.toUpperCase();
 //prompt randomly computerChoice
-
+var robotChoice = alert("Computer choose:" + robot);  
 //compare choices
-
+if (person === robotChoice ) {
+    alert('Its a tie'); 
+    t++ ;
+} else if (
+    (person === 'R' && robotChoice === 'S')
+    (person === 'P' && robotChoice === 'R')
+    (person === 'S' && robotChoice === 'P')
+    ) {
+     alert('Its a Win'); 
+    w++ ;   
+} else{
+    alert('You Lose');
+    l++;
+}
 
 //display results (won , tied or lost the round)
 
