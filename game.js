7cef75dadabe43d4bbdfe9a55ac03edbadec7a8c
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-12 20:28:20 -0500

    code

--- game.js
+++ game.js
@@ -1,25 +1,26 @@
+
+ var game = function () {   
 //define Rock / Paper / Scissors
 var choice = ['R' , 'P', 'S']
 //define variable for computerChoice
 var robot = Math.floor(Math.random() * choice.length);
+var robotChoice = choice[robot];
 //define variable for humanChoice
 //var humanChoice = 
 //define variables for humans (wins, losses, ties)
-var w = 0;
-var l = 0; 
-var t = 0;
+
 //prompt playerChoice
 var person = prompt('Enter R, P, or S:');
 person = person.toUpperCase();
 //prompt randomly computerChoice
-var robotChoice = alert("Computer choose:" + robot);  
+alert("Computer choose:" + robotChoice);  
 //compare choices
 if (person === robotChoice ) {
     alert('Its a tie'); 
     t++ ;
-} else if (
-    (person === 'R' && robotChoice === 'S')
-    (person === 'P' && robotChoice === 'R')
+} else if ( 
+    (person === 'R' && robotChoice === 'S') ||
+    (person === 'P' && robotChoice === 'R') ||
     (person === 'S' && robotChoice === 'P')
     ) {
      alert('Its a Win'); 
@@ -29,13 +30,17 @@ if (person === robotChoice ) {
     l++;
 }
 
-//display results (won , tied or lost the round)
-
 //display total stats (wins, Losses, Ties)
-
+alert('Stats: \n  Wins: ' + w + '\n Losses: ' + l + '\n Ties: ' + t );
 //ask to play again
+
+var playAgain = confirm('Play Again') 
+    if (playAgain ){
+        game();
+    }
     //if play agin (restart)
     //else stop game
-
+};
+game();
 
 
