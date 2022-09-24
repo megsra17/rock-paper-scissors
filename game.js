051a0144047b1b912f19eafb0af1d1e5019ee371
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-24 12:23:12 -0500

    added html/JS

--- game.js
+++ game.js
@@ -1,71 +1,79 @@
-var rock = document.querySelector('#rock');
-var paper = document.querySelector('#paper');
-var scissors = document.querySelector('#scissors')
+var rockImg = document.querySelector('#rock');
+var paperImg = document.querySelector('#paper');
+var scissorsImg = document.querySelector('#scissors')
+var humanScoreEl = document.querySelector('#human-score')
+var computerScoreEl = document.querySelector('#computer-score')
 
-//define variables for humans (wins, losses, ties)
-var w = 0;
-var t = 0;
-var l = 0;
+
 
 //define Rock / Paper / Scissors
 var choice = ['R' , 'P', 'S']
+var humanScore = 0
+var computerScore = 0
 
-
- var game = function () {   
-
-//define variable for computerChoice
-var robot = Math.floor(Math.random() * choice.length);
-var robotChoice = choice[robot];
-//define variable for humanChoice
-//var humanChoice = 
-
-//prompt playerChoice
-var person = prompt('Enter R, P, or S:');
-person = person.toUpperCase();
-//prompt randomly computerChoice
-alert("Computer choose:" + robotChoice);  
-//compare choices
-if (person === robotChoice ) {
-    alert('Its a tie'); 
-    t++ ;
-} else if ( 
-    (person === 'R' && robotChoice === 'S') ||
-    (person === 'P' && robotChoice === 'R') ||
-    (person === 'S' && robotChoice === 'P')
-    ) {
-     alert('Its a Win'); 
-    w++ ;   
-} else{
-    alert('You Lose');
-    l++;
+function startRound(){
+    console.log("startround")
 }
 
-//display total stats (wins, Losses, Ties)
-alert('Stats: \n  Wins: ' + w + '\n Losses: ' + l + '\n Ties: ' + t );
-//ask to play again
-
-var playAgain = confirm('Play Again') 
-    if (playAgain){
-        game();
-    }
-    //if play agin (restart)
-    //else stop game
-};
-game();
-
-/*function greaterThan(x,y){
-if(x>y){
-return x;
-}else {
-    return y;
-}
-}*/
+rockImg.addEventListener('click', startRound)
+scissorsImg.addEventListener('click', startRound)
+paperImg.addEventListener('click', startRound)
 
 
-function sumNumber(x){
-    var sum = 0;
-    for(var i=1; i <= x; i++){
-        sum += i;
-    }
-    return sum
-}
+//  var game = function () {   
+
+// //define variable for computerChoice
+// var robot = Math.floor(Math.random() * choice.length);
+// var robotChoice = choice[robot];
+// //define variable for humanChoice
+// //var humanChoice = 
+
+// //prompt playerChoice
+// var person = prompt('Enter R, P, or S:');
+// person = person.toUpperCase();
+// //prompt randomly computerChoice
+// alert("Computer choose:" + robotChoice);  
+// //compare choices
+// if (person === robotChoice ) {
+//     alert('Its a tie'); 
+//     t++ ;
+// } else if ( 
+//     (person === 'R' && robotChoice === 'S') ||
+//     (person === 'P' && robotChoice === 'R') ||
+//     (person === 'S' && robotChoice === 'P')
+//     ) {
+//      alert('Its a Win'); 
+//     w++ ;   
+// } else{
+//     alert('You Lose');
+//     l++;
+// }
+
+// //display total stats (wins, Losses, Ties)
+// alert('Stats: \n  Wins: ' + w + '\n Losses: ' + l + '\n Ties: ' + t );
+// //ask to play again
+
+// var playAgain = confirm('Play Again') 
+//     if (playAgain){
+//         game();
+//     }
+//     //if play agin (restart)
+//     //else stop game
+// };
+// game();
+
+// /*function greaterThan(x,y){
+// if(x>y){
+// return x;
+// }else {
+//     return y;
+// }
+// }*/
+
+// function sumNumber(x){
+//     var sum = 0;
+//     for(var i=1; i <= x; i++){
+//         sum += i;
+//     }
+//     return sum
+// }
