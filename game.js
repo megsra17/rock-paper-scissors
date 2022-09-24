2d967459e5f2d5a14ee3bfb2d245674811744b4b
Megan Rakow <meganrakow@Megans-MBP.lan>
2022-09-24 11:49:11 -0500

    added styles/html

--- game.js
+++ game.js
@@ -49,4 +49,19 @@ var playAgain = confirm('Play Again')
 };
 game();
 
+/*function greaterThan(x,y){
+if(x>y){
+return x;
+}else {
+    return y;
+}
+}*/
 
+
+function sumNumber(x){
+    var sum = 0;
+    for(var i=1; i <= x; i++){
+        sum += i;
+    }
+    return sum
+}
