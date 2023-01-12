fca6b89e916cf4ce9593602a11db8e7fdee696af
Megan Rakow <meganrakow@Megans-MacBook-Pro-2.local>
2023-01-11 20:00:06 -0600

    add css style

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/js/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
